327d4afafb0d1767b2836b166b1f1063ffffaa25
Roland Hill <roleyhill@gmail.com>
2026-08-20 08:47:48 +1000

    Check descriptorCount matches number of Descriptors

--- src/vsg/state/DescriptorSet.cpp
+++ src/vsg/state/DescriptorSet.cpp
@@ -76,10 +76,15 @@ void DescriptorSet::compile(Context& context)
             throw Exception{"Error: invalid DescriptorSet as no setLayout assigned.", VK_INCOMPLETE};
         }
 
-        // if (setLayout->bindings.size() != descriptors.size())
-        // {
-        //     throw Exception{make_string("Error: invalid DescriptorSet as setLayout bindings size (", setLayout->bindings.size(), ") and descriptors size ", descriptors.size(), ") not equal."), VK_INCOMPLETE};
-        // }
+        size_t numDescriptors = 0;
+        for(auto& binding : setLayout->bindings)
+        {
+            numDescriptors += binding.descriptorCount;
+        }
+        if (numDescriptors != descriptors.size())
+        {
+            throw Exception{make_string("Error: invalid DescriptorSet as setLayout bindings size (", numDescriptors, ") and descriptors size ", descriptors.size(), ") not equal."), VK_INCOMPLETE};
+        }
 
         // make sure all the contributing objects are compiled
         setLayout->compile(context);
